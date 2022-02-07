2a40a349b038f4d6bb5f1d408215ce048d733f3e
Etienne Vautherin <etienne@vautherin.com>
2022-02-07 13:52:24 +0100

    Enter/Exit

--- GeoNotification/NotificationCenterDelegate.swift
+++ GeoNotification/NotificationCenterDelegate.swift
@@ -41,12 +41,35 @@ extension NotificationCenterDelegate: UNUserNotificationCenterDelegate {
     }
     
     class func regionNotify(region: CLRegion) {
-        let trigger = UNLocationNotificationTrigger(region: region, repeats: true)
-        let content = UNMutableNotificationContent()
-        content.title = "My First Notification"
-        content.body = "Hello from Notification Service"
-        let request = UNNotificationRequest(identifier: "NotifIdentifier", content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { error in
+        let notificationCenter = UNUserNotificationCenter.current()
+            
+        notificationCenter.removeAllPendingNotificationRequests()
+        
+        region.notifyOnEntry = true
+        region.notifyOnExit = false
+        let triggerEnter = UNLocationNotificationTrigger(region: region, repeats: true)
+        let contentEnter = UNMutableNotificationContent()
+        contentEnter.title = "Enter Notification"
+        contentEnter.body = "You're entering \(region.identifier)"
+        
+        let requestEnter = UNNotificationRequest(identifier: "Enter", content: contentEnter, trigger: triggerEnter)
+        notificationCenter.add(requestEnter) { error in
+            print("regionNotify: \(region)")
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+        
+        
+        region.notifyOnEntry = false
+        region.notifyOnExit = true
+        let triggerExit = UNLocationNotificationTrigger(region: region, repeats: true)
+        let contentExit = UNMutableNotificationContent()
+        contentExit.title = "Exit Notification"
+        contentExit.body = "You're exiting \(region.identifier)"
+        
+        let requestExit = UNNotificationRequest(identifier: "Exit", content: contentExit, trigger: triggerExit)
+        notificationCenter.add(requestExit) { error in
             print("regionNotify: \(region)")
             if let error = error {
                 print("Error: \(error.localizedDescription)")
