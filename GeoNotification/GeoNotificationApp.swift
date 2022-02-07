064f56730980594e9db7f3036e352339bcbed6ec
Etienne Vautherin <etienne@vautherin.com>
2022-02-07 10:29:45 +0100

    Initial Commit

---
+++ GeoNotification/GeoNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoNotificationApp.swift
+//  GeoNotification
+//
+//  Created by Etienne Vautherin on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
